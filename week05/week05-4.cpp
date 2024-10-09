b7bbf24b7ed9a16608c43289a3c5008191ae0946
OwOrider <paz.4613@gmail.com>
2024-10-09 11:50:51 +0800

    week05

---
+++ week05/week05-4.cpp
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+	string s="`124567890-=qwertyuiop[]\\asdfghjkl;'zxcvbnm,./";
+	char c;
+	while(scanf("%c",&c)==1)
+	{
+		c=tolower(c);
+		
+		if(c==' ' || c=='\n')cout << c;
+		else
+		{
+			int i = s.find(c);
+			cout << s[i-2];
+		} 
+	}
+
+}
